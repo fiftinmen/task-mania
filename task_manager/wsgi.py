fc537ed8919dacc86b40100d1aecbea205258e52
fiftinmen <fiftin.men@gmail.com>
2024-06-21 17:14:32 +0300

    First commit. Create Django project. Add whitenoise Add dependencies Add dj_database_url Preconfigure DATABASE Prepare .env

---
+++ task_manager/wsgi.py
@@ -0,0 +1,20 @@
+"""
+WSGI config for task_manager project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+from whitenoise import WhiteNoise
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'task_manager.settings')
+
+application = get_wsgi_application()
+application = WhiteNoise(application, root="/path/to/static/files")
+application.add_files("/path/to/more/static/files", prefix="more-files/")
