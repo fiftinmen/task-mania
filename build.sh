e4d153356b59e214925c3cf01923193a5e7fb83a
fiftinmen <fiftin.men@gmail.com>
2024-06-24 16:43:12 +0300

    remove build.sh from .gitignore

---
+++ build.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+# Postgres позволяет подключиться к удаленной базе указав ссылку на нее после флага -d
+# ссылка подгрузится из переменной окружения, которую нам нужно будет указать на сервисе деплоя
+# дальше мы загружаем в поключенную базу наш sql-файл с таблицами
+make install && psql -a -d $DATABASE_URL -f database.sql
+make makemigrations_and_migrate
+poetry run python manage.py collectstatic --noinput
