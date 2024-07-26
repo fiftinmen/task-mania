cc26357d78038343e2abee107a326e175db48450
fiftinmen <fiftin.men@gmail.com>
2024-07-26 16:38:51 +0300

    another try with --prepend and --global

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 export DEBIAN_FRONTEND=noninteractive
 apt update -y
 apt install pipx -y
-pipx ensurepath
+pipx ensurepath --prepend
+pipx ensurepath --global
 pipx install poetry
 make install
 make make-n-migrate
