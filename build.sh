f0e1d199f07147c33b108ecad542a5fa61866c0f
fiftinmen <fiftin.men@gmail.com>
2024-07-26 16:57:47 +0300

    try 2

--- build.sh
+++ build.sh
@@ -3,6 +3,7 @@ export DEBIAN_FRONTEND=noninteractive
 export $PATH="/root/.local/bin":$PATH
 apt update -y
 apt install pipx -y
+pipx ensurepath
 pipx ensurepath --prepend
 pipx ensurepath --global
 pipx install poetry
